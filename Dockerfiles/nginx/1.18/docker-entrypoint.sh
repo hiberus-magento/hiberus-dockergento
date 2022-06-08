608959e859915e59aab94dc07891c0de8dd318eb
dabad <dabad@hiberus.com>
2022-06-08 18:04:46 +0200

    [Update] Nginx: Removed SSL certificates

--- Dockerfiles/nginx/1.18/docker-entrypoint.sh
+++ Dockerfiles/nginx/1.18/docker-entrypoint.sh
@@ -5,10 +5,4 @@ if [ -f /var/www/conf/nginx/default.conf ]; then
 	sudo cp -f /var/www/conf/nginx/default.conf /etc/nginx/conf.d/
 fi
 
-if [ ! -f "/etc/nginx/certs/nginx.crt" ]; then
-  sudo mkcert -key-file nginx.key -cert-file nginx.crt
-  sudo mv nginx.key nginx.crt /etc/nginx/certs/
-  sudo chown app:app /etc/nginx/certs/nginx.key /etc/nginx/certs/nginx.crt
-fi
-
 exec "$@"
