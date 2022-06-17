9de56af5daf10f0dc9447432001318b718fefa42
Sergio Mendez <smendez@MacBook-Pro-de-usuario.local>
2022-06-17 08:49:43 +0200

    Add setup application script

---
+++ console/hm-completion.bash
@@ -0,0 +1,28 @@
+#/usr/bin/env bash
+
+complete -W "bash \ 
+composer \ 
+create-project \ 
+docker-compose \ 
+docker-stop-all \ 
+down \ 
+exec \ 
+install \ 
+magento \ 
+mirror-container \ 
+mirror-host \ 
+purge \ 
+rebuild \ 
+restart \ 
+set-host \ 
+setup \ 
+ssl \ 
+start \ 
+stop \ 
+test-integration \ 
+test-unit \ 
+transfer-db \ 
+transfer-media \ 
+urn-generate \ 
+watch \ 
+" hm
