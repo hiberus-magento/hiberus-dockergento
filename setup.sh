cfdbf4181887600b2827af02e8ff96a558f337bb
Sergio Méndez <sergiomfg82@gmail.com>
2022-06-24 10:40:57 +0200

    [NEW] start project setup script

--- setup.sh
+++ setup.sh
@@ -1,28 +1,50 @@
 #!/usr/bin/env bash
 
-DIR=$(dirname -- "$(readlink -f -- "$0")")
-EXECUTABLE="source ${DIR}/console/hm-completion.bash"
-if [ "$(uname)" == "Darwin" ]; then
-    SOURCE_FILE="${HOME}/.zshrc"
-else
-    SOURCE_FILE="${HOME}/.bashrc"
+blue="\033[0;34m"
+green="\033[0;32m"
+red="\033[0;31m"
+brown="\033[0;33m"
+colorReset="\033[0m"
+
+if ! command -v jq &>/dev/null; then
+    echo -e "${green}Hiberus docker needs to install ${brown}jq${green} library${colorReset}"
+
+    state="continue"
+    while [[ "$state" == "continue" ]]; do
+        printf "${blue}Do you want to install ${brown}jq${blue} in your computer? [y/n] ${colorReset}"
+        read -r yn
+
+        case $yn in
+        [Nn]*)
+            echo -e "${green}You can get information about jq instalation in:${colorReset}"
+            echo -e "${blue}https://stedolan.github.io/jq/download/${colorReset}"
+            exit 1 ;;
+        [Yy]*)
+            # Install on MacOS
+            if [ "$(uname)" == "Darwin" ]; then
+                if ! command -v brew &>/dev/null; then
+                    echo -e "${red}Error: Brew is required. Please install it and try again.${colorReset}"
+                    exit 1
+                fi
+                brew install jq
+            # Install on Linux
+            else
+                sudo sudo apt-get install jq
+            fi
+            break
+            ;;
+        *) echo "Please answer yes or no." ;;
+        esac
+    done
 fi
 
-# Show copy
-source "${DIR}/console/tasks/copyright.sh"
-
-# Compose string with all commands
-COMMANDS=""
-for script in "${DIR}/console/commands/"*.sh; do
-    COMMAND_BASENAME=$(basename "${script}")
-    COMMAND_NAME=${COMMAND_BASENAME%.sh}
-    COMMANDS="${COMMANDS}${COMMAND_NAME} \\ \n"
-done
-
-# Write autocomplete file
-echo -e "#!/usr/bin/env bash\n\ncomplete -W \"${COMMANDS}\" hm" >"${DIR}"/console/hm-completion.bash
-
-# Write source sentence in .zshrc
-if ! grep -q "${EXECUTABLE}" "$SOURCE_FILE"; then
-    echo "${EXECUTABLE}" >>"$SOURCE_FILE"
+if [ -f ~/hm ]; then
+    echo -e "${red}Hiberus docker already exits in your computer${colorReset}"
+    exit 1
 fi
+
+git clone https://github.com/hiberus-magento/hiberus-dockergento.git ~/hm
+sudo ln -s "$HOME"/hm/bin/run /usr/local/bin/hm
+
+# Execute generatt completion for autocomplete commands
+"$HOME"/hm/generate_completion.sh
