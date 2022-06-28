74870698976c37229e4f7e76d2ce2cddaa42b25d
Sergio Méndez <sergiomfg82@gmail.com>
2022-06-28 15:15:07 +0200

    [Update] fixes code sniffer

--- console/commands/transfer-db.sh
+++ console/commands/transfer-db.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+# shellcheck source=/dev/null
+source "$COMPONENTS_DIR"/input_info.sh
+
 sshHost="ssh.eu-3.magento.cloud"
 sshUser=""
 sqlHost="database.internal"
@@ -8,17 +11,17 @@ sqlUser="mysql"
 sqlDb="main"
 sqlPassword=""
 
-printf "${GREEN}Database transfer assistant: ${COLOR_RESET}\n"
+print_info "Database transfer assistant: \n"
 
 # Check php container
 if [ -z "$(docker ps | grep php)" ]; then
-    printf "${RED}Error: PHP container is not running!${COLOR_RESET}\n"
+    print_error "Error: PHP container is not running!\n"
     exit 1
 fi
 
 # Check mysql container
 if [ -z "$(docker ps | grep mysql)" ]; then
-    printf "${RED}Error: MySQL container is not running!${COLOR_RESET}\n"
+    print_error "Error: MySQL container is not running!\n"
     exit 1
 fi
 
@@ -47,8 +50,8 @@ for i in "$@"; do
 done
 
 # Request SSH credentials
-read -p "Do you need to use SSH tunneling? [Yn]: " sshTunnel
-if [ -z "$sshTunnel" ] || [ $sshTunnel == "Y" ] || [ $sshTunnel == "y" ]; then
+read -r -p "Do you need to use SSH tunneling? [Yn]: " sshTunnel
+if [ -z "$sshTunnel" ] || [ "$sshTunnel" == "Y" ] || [ "$sshTunnel" == "y" ]; then
     read -p "SSH Host [Default: '${sshHost}']: " inputSshHost
     read -p "SSH User [Default: '${sshUser}']: " inputSshUser
     sshHost=${inputSshHost:-${sshHost}}
@@ -71,28 +74,29 @@ sqlPassword=${inputSqlPassword:-${sqlPassword}}
 # Prepare password
 [ -z "$sqlPassword" ] && sqlPassword="" || sqlPassword="-p'$sqlPassword'"
 
-printf "${GREEN}You are going to transfer database from [${sshHost}:${sqlHost}] to [LOCALHOST]. ${COLOR_RESET}\nPress any key continue..."
-read
+print_info "You are going to transfer database from [${sshHost}:${sqlHost}] to [LOCALHOST].\n"
+print_default "Press any key continue..."
+read -r
 
 # Check required data
 if [ -z "$sqlHost" ] || [ -z "$sqlUser" ] || [ -z "$sqlDb" ]; then
-    printf "${RED}Error: Please enter all required data${COLOR_RESET}\n"
+    print_error "Error: Please enter all required data\n"
     exit 1
 fi
 
-printf "${GREEN}Creating database dump from origin server...\n"
+print_info "Creating database dump from origin server...\n"
 
 # Create database dump from origin server (WITHOUT SSH TUNNEL)
 if [ -z "$sshHost" ]; then
 
     # Create dump into mysql container
-    docker-compose exec db bash -c "mysqldump -h'${sqlHost}' -u'${sqlUser}' ${sqlPassword} ${sqlDb} --ignore-table=${sqlDb}.core_config_data | sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/' | gzip -9 > /tmp/db.sql.gz"
+    docker-compose exec db bash -c "mysqldump -h'$sqlHost' -u'$sqlUser' $sqlPassword $sqlDb --ignore-table=$sqlDb.core_config_data | sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/' | gzip -9 > /tmp/db.sql.gz"
 
 # Create database dump from origin server (WITH SSH TUNNEL)
 else
 
     # Create dump
-    ssh ${sshUser}@${sshHost} "mysqldump -h'${sqlHost}' -u'${sqlUser}' ${sqlPassword} ${sqlDb} --ignore-table=${sqlDb}.core_config_data | sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/' | gzip -9 > /tmp/db.sql.gz"
+    ssh ${sshUser}@${sshHost} "mysqldump -h'$sqlHost' -u'$sqlUser' $sqlPassword $sqlDb --ignore-table=$sqlDb.core_config_data | sed -e 's/DEFINER[ ]*=[ ]*[^*]*\*/\*/' | gzip -9 > /tmp/db.sql.gz"
 
     # Download dump
     scp ${sshUser}@${sshHost}:/tmp/db.sql.gz .
@@ -102,21 +106,21 @@ else
 
 fi
 
-printf "${GREEN}Restoring database dump into localhost...\n"
+print_info "Restoring database dump into localhost...\n"
 
 # Restore dump
-docker-compose exec db bash -c 'zcat /tmp/db.sql.gz | mysql -f -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE'
+docker-compose exec db bash -c "zcat /tmp/db.sql.gz | mysql -f -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE"
 
 # Reindex Magento
-read "Do you want to reindex Magento? [Y/n]: " reindexMagento
+read -r "Do you want to reindex Magento? [Y/n]: " reindexMagento
 if [ -z "$reindexMagento" ] || [ "$reindexMagento" == 'Y' ] || [ "$reindexMagento" == 'y' ]; then
     docker-compose exec phpfpm bin/magento indexer:reindex
 fi
 
 # Clear Magento cache
-read "Do you want to clear Magento cache? [Y/n]: " clearMagento
+read -r "Do you want to clear Magento cache? [Y/n]: " clearMagento
 if [ -z "$clearMagento" ] || [ "$clearMagento" == 'Y' ] || [ "$clearMagento" == 'y' ]; then
     docker-compose exec phpfpm bin/magento c:f
 fi
 
-printf "${GREEN} All done!${COLOR_RESET}\n"
+print_info " All done!\n"
--- console/commands/transfer-media.sh
+++ console/commands/transfer-media.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
+# shellcheck source=/dev/null
+source "$COMPONENTS_DIR"/input_info.sh
+
 sshHost="ssh.eu-3.magento.cloud"
 sshPath="/app/pub/media"
 sshUser=""
 
-printf "${GREEN}Media transfer assistant: ${COLOR_RESET}\n"
+print_info "Media transfer assistant: \n"
 
 for i in "$@"; do
     case $i in
@@ -23,35 +26,35 @@ for i in "$@"; do
 done
 
 # Request data
-read -p "SSH Host [Default: '${sshHost}']: " inputSshHost
-read -p "SSH User [Default: '${sshUser}']: " inputSshUser
-read -p "SSH pub/media path [Default: '${sshPath}']: " inputSshPath
+read -p "SSH Host [Default: '$sshHost']: " inputSshHost
+read -p "SSH User [Default: '$sshUser']: " inputSshUser
+read -p "SSH pub/media path [Default: '$sshPath']: " inputSshPath
 sshHost=${inputSshHost:-${sshHost}}
 sshUser=${inputSshUser:-${sshUser}}
 sshPath=${inputSshPath:-${sshPath}}
 
 if [ -z "$sshUser" ] || [ -z "$sshPath" ] || [ -z "$sshHost" ]; then
-    printf "${RED}Error: Please enter all required data${COLOR_RESET}\n"
+    print_error "Error: Please enter all required data\n"
     exit 1
 fi
 
 # Request confirmation
-printf "${GREEN}You are going to transfer files from [${sshHost}] to [LOCALHOST]. ${COLOR_RESET}\nPress any key continue..."
-read
+print_info "You are going to transfer files from [${sshHost}] to [LOCALHOST]. \nPress any key continue..."
+read -r
 
 # Check rsync data
 if ! command -v rsync &>/dev/null; then
-    printf "${RED}Error: Rsync command not available${COLOR_RESET}\n"
+    print_error "Error: Rsync command not available\n"
     exit 1
 fi
 
 # Check local pub/media directory
 if [ ! -d "./pub/media" ]; then
-    printf "${RED}Error: Local pub/media directory not found. Are you in the correct path?${COLOR_RESET}\n"
+    print_error "Error: Local pub/media directory not found. Are you in the correct path?\n"
     exit 1
 fi
 
 # Transfer pub/media files
 rsync -az --ignore-existing --exclude '**/cache' --max-size=10m ${sshUser}@${sshHost}:${sshPath} ./pub
 
-printf "${GREEN}All media content transferred! ${COLOR_RESET}\n"
+print_info "All media content transferred! \n"
