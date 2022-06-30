8af1c5fe2f6abcc058d6dbabfccf6710cd3198b9
dabad <dabad@hiberus.com>
2022-06-30 22:07:10 +0200

    [Update] Fixes SSL script printed literales

--- console/commands/ssl.sh
+++ console/commands/ssl.sh
@@ -11,20 +11,20 @@ else
 fi
 
 if [ -z "$(docker ps | grep hitch)" ]; then
-    print_error "Error: Hitch is not running!"
+    print_error "Error: Hitch is not running!\n"
     exit
 fi
 
-print_info "$(printf "Generating SSL certificates for domain '%s'..." "${DOMAIN}")"
+print_info "$(printf "Generating SSL certificates for domain '%s'..." "${DOMAIN}")\n"
 
 # Check if command "mkcert" exists
 if ! command -v mkcert &>/dev/null; then
-    print_error "Required 'mkcert' command not found. Trying to install..."
+    print_error "Required 'mkcert' command not found. Trying to install...\n"
 
     # Install on MacOS
     if [ "$(uname)" == "Darwin" ]; then
         if ! command -v brew &>/dev/null; then
-            print_error "Error: Brew is required. Please install it and try again."
+            print_error "Error: Brew is required. Please install it and try again.\n"
             exit 1
         fi
         brew install mkcert nss
@@ -44,16 +44,16 @@ if ! command -v mkcert &>/dev/null; then
 fi
 
 if ! command -v mkcert &>/dev/null; then
-    print_error "Error during 'mkcert' installation. Please do it manually and try again..."
+    print_error "Error during 'mkcert' installation. Please do it manually and try again...\n"
     exit 1
 fi
 
 # Generate mkcert certificate
-print_info "Installing SSL certificate into docker environment..."
+print_info "Installing SSL certificate into docker environment...\n"
 mkcert -cert-file ssl.crt -key-file ssl.key ${DOMAIN} localhost 127.0.0.1 0.0.0.0 ::1
 cat ssl.crt ssl.key >ssl.pem && rm ssl.crt ssl.key
 docker cp ./ssl.pem "$(docker-compose ps -q hitch | awk '{print $1}')":/etc/hitch/testcert.pem
 docker-compose exec -T -u root hitch chown hitch /etc/hitch/testcert.pem
 docker-compose restart hitch
 
-print_info "SSL certificate installed! Remember to restart your browser"
+print_info "SSL certificate installed! Remember to restart your browser\n"
