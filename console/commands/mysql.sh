6b9bfd57eeda63f0cd49dc9fb34955aba5498da9
dabad <dabad@hiberus.com>
2022-07-07 10:18:21 +0200

    [Update] Use environment vars for mysql commands

--- console/commands/mysql.sh
+++ console/commands/mysql.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-yml_file="$MAGENTO_DIR/docker-compose.yml"
-path_to_mysql_keys="services_db_environment_MYSQL"
-look_at_task="$TASKS_DIR/look_at_yml.sh"
+MYSQL_QUERY=${*:1}
+if [ ! -z "$MYSQL_QUERY" ]; then
+  MYSQL_QUERY="\"$MYSQL_QUERY\""
+fi
 
-# Get credentials
-user=$($look_at_task "$yml_file" "${path_to_mysql_keys}_USER")
-password=$($look_at_task "$yml_file" "${path_to_mysql_keys}_PASSWORD")
-database=$($look_at_task "$yml_file" "${path_to_mysql_keys}_DATABASE")
-
-$COMMAND_BIN_NAME docker-compose exec -T db mysql -u"$user" -p"$password" "$database" "$@"
+docker-compose exec db bash -c "mysql -u\"\$MYSQL_USER\" -p\"\$MYSQL_PASSWORD\" \"\$MYSQL_DATABASE\" $MYSQL_QUERY"
