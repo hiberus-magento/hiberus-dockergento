8399f4a885027bfc7e1c665a3c77d7dd553852e1
dabad <dabad@hiberus.com>
2022-07-25 13:53:16 +0200

    [Hotfix] Mysql command fix (-T flag for docker-compose)

--- console/commands/mysql.sh
+++ console/commands/mysql.sh
@@ -6,4 +6,4 @@ if [ ! -z "$MYSQL_QUERY" ]; then
   MYSQL_QUERY="\"$MYSQL_QUERY\""
 fi
 
-docker-compose exec db bash -c "mysql -u\"\$MYSQL_USER\" -p\"\$MYSQL_PASSWORD\" \"\$MYSQL_DATABASE\" $MYSQL_QUERY"
+docker-compose exec -T db bash -c "mysql -u\"\$MYSQL_USER\" -p\"\$MYSQL_PASSWORD\" \"\$MYSQL_DATABASE\" $MYSQL_QUERY"
