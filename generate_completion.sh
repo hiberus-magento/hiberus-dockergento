732d2eee6d79099dbab48e2d98b49825becfb7dd
sergiomfg <smendez@hiberus.com>
2022-08-16 11:58:36 +0200

    Create configuration file if not exist

--- generate_completion.sh
+++ generate_completion.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 dir=$(dirname -- "$(readlink -f -- "$0")")
-executable="source ${dir}/console/hm-completion.bash"
+executable="source $dir/console/hm-completion.bash"
 
 if [ "$(uname)" == "Darwin" ]; then
     sourceFile="$HOME/.zshrc"
@@ -14,16 +14,21 @@ source "$dir/console/tasks/copyright.sh"
 
 # Compose string with all commands
 commands=""
-for script in "${dir}/console/commands/"*.sh; do
-    commandBaseName=$(basename "${script}")
+for script in "$dir/console/commands/"*.sh; do
+    commandBaseName=$(basename "$script")
     commandName=${commandBaseName%.sh}
     commands="${commands}${commandName} \\ \n"
 done
 
 # Write autocomplete file
-echo -e "#!/usr/bin/env bash\n\ncomplete -W \"${commands}\" hm" >"${dir}"/console/hm-completion.bash
+echo -e "#!/usr/bin/env bash\n\ncomplete -W \"$commands\" hm" >"$dir"/console/hm-completion.bash
+
+# Create source file if not exist
+if [ ! -z $sourceFile ]; then
+    touch $sourceFile
+fi
 
 # Write source sentence in .zshrc
-if ! grep -q "${executable}" "$sourceFile"; then
-    echo "${executable}" >>"$sourceFile"
+if ! grep -q "$executable" "$sourceFile"; then
+    echo "$executable" >>"$sourceFile"
 fi
