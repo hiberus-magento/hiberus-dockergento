d8490864112970339562bd189b71df74b4518992
Sergio M <smendez@hiberus.com>
2023-03-14 17:47:30 +0100

    [WIP] - Creating flow when import database during setup

--- console/tasks/magento_installation.sh
+++ console/tasks/magento_installation.sh
@@ -2,16 +2,27 @@
 set -euo pipefail 
 
 source "$COMPONENTS_DIR"/input_info.sh
+source "$COMPONENTS_DIR"/print_message.sh
 
 #
 # Ask sql file and launch mysql import process
 #
 create_database() {
     if [[ $# -gt 0 && -n $1 ]]; then
+        if [[ -e $MAGENTO_DIR/app/code ]]; then
+            mv $MAGENTO_DIR/app/code $MAGENTO_DIR/app/code-tmp-for-install
+        fi
+        print_info "Importing database: "
+        print_default "$1\n"
         "$COMMANDS_DIR"/mysql.sh < "$1"
         "$COMMANDS_DIR"/mysql.sh -q "DELETE FROM admin_user;"
+        "$COMMANDS_DIR"/install.sh
+        if [[ -e $MAGENTO_DIR/app/code-tmp-for-install ]]; then
+            mv $MAGENTO_DIR/app/code-tmp-for-install $MAGENTO_DIR/app/code
+        fi
+    else
+        "$COMMANDS_DIR"/install.sh
     fi
-    "$COMMANDS_DIR"/install.sh "$DOMAIN"
 }
 
 # Magento installation and database
