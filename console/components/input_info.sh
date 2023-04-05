df8b8376308167653159d7a3e5542b6e03cb48f6
Sergio M <smendez@hiberus.com>
2023-04-05 17:09:31 +0200

    [NEW] - Create a custom_select component

--- console/components/input_info.sh
+++ console/components/input_info.sh
@@ -57,31 +57,22 @@ get_magento_version() {
 # Get magento edition
 #
 get_magento_edition() {
-    AVAILABLE_MAGENTO_EDITIONS="community enterprise"
+    available_magento_editions=("community" "enterprise")
 
     if [[ $# -eq 0 || -z "$1" ]]; then
-        print_question "Magento edition:\n"
-        select MAGENTO_EDITION in ${AVAILABLE_MAGENTO_EDITIONS}; do
-            if $("$TASKS_DIR/in_list.sh" "$MAGENTO_EDITION" "$AVAILABLE_MAGENTO_EDITIONS"); then
-                break
-            fi
+        label="Magento edition:"
+        custom_select "$label" "${available_magento_editions[@]}"
 
-            if $("$TASKS_DIR/in_list.sh" "$REPLY" "$AVAILABLE_MAGENTO_EDITIONS"); then
-                MAGENTO_EDITION=$REPLY
-                break
-            fi
-            echo "invalid option '$REPLY'"
-        done
+        magento_edition=$REPLY
     else
         if [[ $1 == "community" || $1 == "enterprise" ]]; then
-            MAGENTO_EDITION=$1
+            magento_edition=$1
         else
             print_warning "Edition '$1' is not available.\n"
             get_magento_edition
         fi
     fi
-
-    export MAGENTO_EDITION=$MAGENTO_EDITION
+    export MAGENTO_EDITION=$magento_edition
 }
 
 #
@@ -195,4 +186,50 @@ sed_in_file() {
     else
         sed -i "$sed_regex" "$target_path"
     fi
+}
+
+prepare_options() {
+    local opts=("$@")
+
+    for i in "${!opts[@]}"; do
+        opts[$i]=$(print_info "${opts[$i]}")
+    done
+    echo "length "${#opts[@]}
+    echo "${opts[@]}"
+}
+
+#
+# Select component
+# Example:
+#    options=("Import sql Dump" "Magento installation")
+#    label="How do you want create database?"
+#
+#    custom_select "$label" "${options[@]}"
+#    
+#    if [[ $REPLY == SOME_REPONSE* ]]; then
+#        echo "RESPONSE"
+#        any_action
+#    fi
+#
+custom_select() {
+    local question="$1"
+    shift
+    local opts=("$@")
+
+    for i in "${!opts[@]}"; do
+        opts[$i]=$(print_table "${opts[$i]}")
+    done
+    COLUMNS=1
+    PS3="$(print_default "Option: ")"
+    print_question "\n✅ $question\n"
+    select REPLY in "${opts[@]}"; do
+        if [[ " ${opts[@]} " ==  *" $REPLY "* ]]; then
+            # Remove color codes from selected option
+            response=$(echo -e "$REPLY" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")
+            export REPLY=$response
+            break
+        fi
+        print_warning "\nInvalid option, choose an option\n"
+        print_question "\n✅ $question\n"
+    done
 }
