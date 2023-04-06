56ee47428b474baa27b6ed102a40cd55a8e398c2
Sergio M <smendez@hiberus.com>
2023-04-06 16:59:47 +0200

    [UPDATE] - Add option query in mysql query

--- console/helpers/map_arguments.sh
+++ console/helpers/map_arguments.sh
@@ -4,43 +4,44 @@
 # Change long options to short options and return new line mapped
 #
 parseToShortArguments() {
-    local command_name="$1"
+    local command_name arguments file output short_name long_name array_pos length processed set_output
+    command_name="$1"
     shift # Remove command name of $@
-    local arguments=("$@")
-    local file="$(cat "$DATA_DIR/command_descriptions.json")"
-    local output=()
-
+    arguments=("$@")
+    file="$(cat "$DATA_DIR/command_descriptions.json")"
+    output=()
     # Get number of options for current command
-    local length=$(echo "$file" | jq -r '.["'$command_name'"].opts | length')
+    length=$(echo "$file" | jq -r '.["'"$command_name"'"].opts | length')
 
     if [[ -n $* ]]; then
         # Itera sobre argumento de entrada
         for argument in "${arguments[@]}"; do
-
             # Map argument
             for ((i = 0; i < length; i++)); do
-                local short_name=$(echo "$file" | jq -r '.["'$command_name'"].opts['$i'].name.short')
-                local long_name=$(echo "$file" | jq -r '.["'$command_name'"].opts['$i'].name.long')
-                local array_pos=${#output[@]}
-
+                short_name=$(echo "$file" | jq -r '.["'"$command_name"'"].opts['$i'].name.short')
+                long_name=$(echo "$file" | jq -r '.["'"$command_name"'"].opts['$i'].name.long')
+                array_pos=${#output[@]}
+                
                 # Replace long by short
                 processed=$(echo "$argument" |\
-
-                    sed s/--$long_name/-$short_name/ |\
-                    sed 's/-'$short_name'=/-'$short_name' /')
+                    sed 's/--'"$long_name"'/-'"$short_name"'/' |\
+                    sed 's/-'"$short_name"'=/-'"$short_name"' /')
             done
-
-            if [[ -z ${processed:=0} ]]; then
+            if [[ -n ${processed:=""} ]]; then
                # Save mapped argument
                 for arg in "${processed[@]}"; do
-                    output[$array_pos]="$arg"
+                    output[array_pos]="$arg"
                     array_pos=$[array_pos + 1]
                 done  
             fi
-            
         done
+        if [[ ${#output[@]} -gt 0 ]]; then
+            export MAPPED=true
+        else
+            export MAPPED=false
+        fi
         export ARGS_PROCESSED="${output:-$}"
     else
-        export ARGS_PROCESSED=false
+        export MAPPED=false
     fi
 }
