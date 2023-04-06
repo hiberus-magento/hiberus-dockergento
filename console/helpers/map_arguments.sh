d1fb864322b7cfb122c094d8a1655e8b533cd78d
Sergio M <smendez@hiberus.com>
2023-04-06 16:55:52 +0200

    [UPDATE] - Improve arguments mapped

--- console/helpers/map_arguments.sh
+++ console/helpers/map_arguments.sh
@@ -6,21 +6,41 @@
 parseToShortArguments() {
     local command_name="$1"
     shift # Remove command name of $@
-    local arguments="$@"
+    local arguments=("$@")
     local file="$(cat "$DATA_DIR/command_descriptions.json")"
+    local output=()
 
     # Get number of options for current command
-    length=$(echo "$file" | jq -r '.["'$command_name'"].opts | length')
-    
-    for ((i = 0; i < length; i++)); do
-        short_name=$(echo "$file" | jq -r '.["'$command_name'"].opts['$i'].name.short')
-        long_name=$(echo "$file" | jq -r '.["'$command_name'"].opts['$i'].name.long')
+    local length=$(echo "$file" | jq -r '.["'$command_name'"].opts | length')
 
-        # Replace long by short
-        arguments=$(echo "$arguments" |\
-            sed s/--$long_name/-$short_name/ |\
-            sed 's/-'$short_name'=/-'$short_name' /')
-    done
+    if [[ -n $* ]]; then
+        # Itera sobre argumento de entrada
+        for argument in "${arguments[@]}"; do
 
-   echo "$arguments"
+            # Map argument
+            for ((i = 0; i < length; i++)); do
+                local short_name=$(echo "$file" | jq -r '.["'$command_name'"].opts['$i'].name.short')
+                local long_name=$(echo "$file" | jq -r '.["'$command_name'"].opts['$i'].name.long')
+                local array_pos=${#output[@]}
+
+                # Replace long by short
+                processed=$(echo "$argument" |\
+
+                    sed s/--$long_name/-$short_name/ |\
+                    sed 's/-'$short_name'=/-'$short_name' /')
+            done
+
+            if [[ -z ${processed:=0} ]]; then
+               # Save mapped argument
+                for arg in "${processed[@]}"; do
+                    output[$array_pos]="$arg"
+                    array_pos=$[array_pos + 1]
+                done  
+            fi
+            
+        done
+        export ARGS_PROCESSED="${output:-$}"
+    else
+        export ARGS_PROCESSED=false
+    fi
 }
