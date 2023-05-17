7a354e699d5eca6ee33a19c53a07ebd08734426e
Sergio M <smendez@hiberus.com>
2023-05-17 21:43:27 +0200

    Print help of custom command from command_descriptions project file

--- console/helpers/print_usage.sh
+++ console/helpers/print_usage.sh
@@ -8,10 +8,15 @@ source "$COMPONENTS_DIR"/print_message.sh
 #
 get_usage() {
     local command_name=$1
-    local command_info=$(jq -r '.["'$command_name'"]' < "$DATA_DIR/command_descriptions.json")
+    local custom_command_descriptions=""
+    local command_info=$(jq -r '.["'$command_name'"]' "$DATA_DIR/command_descriptions.json")
+
+    if [[ $command_info == null && -f "$CUSTOM_COMMANDS_DIR/command_descriptions.json" ]]; then
+        command_info=$(jq -r 'try (.["'$command_name'"]) catch false' "$CUSTOM_COMMANDS_DIR/command_descriptions.json")
+    fi
+
     local usage_property=$(echo "$command_info" | jq -r '.usage')
     local params=$(echo "$command_info" | jq -r '. | if length > 0 then keys[] else false end')
-
 
     if [[ $usage_property != null ]]; then
         print_info "Usage: "
